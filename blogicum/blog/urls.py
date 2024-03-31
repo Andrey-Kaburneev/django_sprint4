0f54fcd2b03ba2454b4d5292ea9767ff50d0c678
Андрей Кабурнеев <kaburneew@yandex.ru>
2024-03-31 20:57:46 +0500

    pep8

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -6,7 +6,11 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='index'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path(
+        'posts/<int:pk>/',
+        views.PostDetailView.as_view(),
+        name='post_detail'
+    ),
     path('posts/create/', views.PostCreateView.as_view(), name='create_post'),
     path(
         'posts/<int:pk>/edit/',
